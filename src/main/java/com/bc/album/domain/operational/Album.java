0a6c6ae0c4ca6d7accace2f72522bf2fdc65ab95
Carmona <33721954+alvcarpal@users.noreply.github.com>
2025-06-03 17:33:43 +0200

    Update Album.java

--- src/main/java/com/bc/album/domain/operational/Album.java
+++ src/main/java/com/bc/album/domain/operational/Album.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Album {
   private Integer id;
+  private String author;
   private Integer userId;
   private String title;
   private List<Photo> photos = new ArrayList<>();
